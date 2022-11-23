be3644266afcf3a7788cdac842f712f66f180fbc
dongjihui666 <dongjihui0320@gmail.com>
2022-11-23 22:08:13 +0900

    SysRoleController.java 新增了分页查询,逻辑删除

--- service-system/src/main/java/com/dong/system/controller/SysRoleController.java
+++ service-system/src/main/java/com/dong/system/controller/SysRoleController.java
@@ -1,10 +1,15 @@
 package com.dong.system.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.dong.common.result.Result;
 import com.dong.model.system.SysRole;
+import com.dong.model.vo.SysRoleQueryVo;
 import com.dong.system.service.SysRoleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.ibatis.io.ResolverUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +31,7 @@ public class SysRoleController {
         return Result.ok(list);
     }
     // 2 逻辑删除接口
+    @ApiOperation("逻辑删除接口")
     @DeleteMapping("remove/{id}")
     public Result removeRole(@PathVariable Long id){
         boolean isSuccess = sysRoleService.removeById(id);
@@ -34,5 +40,52 @@ public class SysRoleController {
         }else {
             return Result.fail("删除失败");
         }
+    }
+    // 3 条件分页查询
+    @ApiOperation("条件分页查询")
+    @GetMapping("/{page}/{limit}")
+    public Result index(
+            // sysRoleQueryVo 分页的条件
+            @ApiParam(name = "page", value = "当前页码", required = true)
+            @PathVariable Long page,
+            @ApiParam(name = "limit", value = "每页记录数",required = true)
+            @PathVariable Long limit,
+            @ApiParam(name = "roleQueryVc",value = "查询对象", required = false)
+            SysRoleQueryVo sysRoleQueryVo){
+        // 创建分页查询
+        Page<SysRole> pageParam = new Page<>(page, limit);
+        // 调用service方法实现
+        // 传入配置对象,传入条件
+        IPage<SysRole> pageModel = sysRoleService.selectPage(pageParam, sysRoleQueryVo);
+        // 返回
+        return Result.ok(pageModel);
+    }
+    // 获取角色
+    @ApiOperation(value = "获取角色")
+    @GetMapping("/get/{id}")
+    public Result get(@PathVariable Long id){
+        SysRole role = sysRoleService.getById(id);
+        return Result.ok(role);
+    }
+    // 新增角色
+    @ApiOperation(value = "新增角色")
+    @PostMapping("/save")
+    public Result save(@RequestBody SysRole role){
+        sysRoleService.save(role);
+        return Result.ok("新增角色成功");
+    }
+    // 修改角色
+    @ApiOperation(value = "修改角色")
+    @PostMapping("/update")
+    public Result updateById(@RequestBody SysRole role){
+        sysRoleService.save(role);
+        return Result.ok("修改角色成功");
+    }
+    // 根据ID列表删除
+    @ApiOperation(value = "根据ID列表删除")
+    @DeleteMapping("/batchRemove")
+    public Result batchRemove(@RequestBody List<Long> idList){
+        sysRoleService.removeByIds(idList);
+        return Result.ok("删除id成功");
     }
 }
--- service-system/src/main/java/com/dong/system/service/impl/SysRoleServiceImpl.java
+++ service-system/src/main/java/com/dong/system/service/impl/SysRoleServiceImpl.java
@@ -1,7 +1,11 @@
 package com.dong.system.service.impl;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.dong.model.system.SysRole;
+import com.dong.model.vo.SysRoleQueryVo;
 import com.dong.system.mapper.SysRoleMapper;
 import com.dong.system.service.SysRoleService;
 import org.springframework.stereotype.Service;
@@ -10,4 +14,10 @@ import org.springframework.stereotype.Service;
 public class SysRoleServiceImpl extends ServiceImpl<SysRoleMapper, SysRole> implements SysRoleService {
 
 
+    // 条件分页查询
+    @Override
+    public IPage<SysRole> selectPage(Page<SysRole> pageParam, SysRoleQueryVo roleQueryVo) {
+        IPage<SysRole> pageModel = baseMapper.selectPage(pageParam, roleQueryVo);
+        return pageModel;
+    }
 }
